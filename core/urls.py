97cc784ccd6091e37785396979a1cb2195292904
Candice Han <jeeyoung.han82@gmail.com>
2020-07-28 09:37:06 +0200

    Update cart

--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('add-to-cart/<slug>/', views.add_to_cart, name="add-to-cart"),
     path('remove-from-cart/<slug>/',
          views.remove_from_cart, name="remove-from-cart"),
+    path('remove-item-from-cart/<slug>/',
+         views.remove_single_item_from_cart, name="remove-single-item-from-cart"),
 ]
